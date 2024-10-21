0b15c0885d9baf1df842202d711ae2df770774cb
Vepohs <la227630@student.helha.be>
2024-10-21 19:40:49 +0200

    push error

--- app/src/main/java/com/example/gestionpoints/controllers/Fragments/FooterFragment.java
+++ app/src/main/java/com/example/gestionpoints/controllers/Fragments/FooterFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -15,10 +16,13 @@ public class FooterFragment extends Fragment {
 
     public interface FooterListener {
         void onAddButtonClick();
+
         void onDeleteButtonClick();
     }
 
-    private FooterListener listener;
+    private FooterListener mListener;
+    private Button mAddBtn;
+    private Button mDeleteBtn;
 
     @Override
     public void onAttach(Context context) {
@@ -26,7 +30,7 @@ public class FooterFragment extends Fragment {
         super.onAttach(context);
         // Vérifier si l'activité implémente l'interface
         if (context instanceof FooterListener) {
-            listener = (FooterListener) context;
+            mListener = (FooterListener) context;
         } else {
             throw new ClassCastException(" doit implémenter FooterListener");
         }
@@ -37,26 +41,38 @@ public class FooterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_footer, container, false);
-        setupButtonListeners(v);
+        retrieveView(v);
+        setData();
+        setupButtonListeners(mAddBtn, mDeleteBtn);
         return v;
     }
 
-    private void setupButtonListeners(View v) {
-        v.findViewById(R.id.addBtn).setOnClickListener(this::handleAddButtonClick);
-        v.findViewById(R.id.deleteBtn).setOnClickListener(this::handleDeleteButtonClick);
+    private void setData() {
+        mAddBtn.setText("Ajouter");
+        mDeleteBtn.setText("Supprimer");
+    }
+
+    private void retrieveView(View v) {
+        mAddBtn = v.findViewById(R.id.addBtn);
+        mDeleteBtn = v.findViewById(R.id.deleteBtn);
+    }
+
+    private void setupButtonListeners(Button addBtn, Button deleteBtn) {
+        addBtn.setOnClickListener(this::handleAddButtonClick);
+        deleteBtn.setOnClickListener(this::handleDeleteButtonClick);
     }
 
     private void handleAddButtonClick(View v) {
-        if (listener != null) {
-            listener.onAddButtonClick();
+        if (mListener != null) {
+            mListener.onAddButtonClick();
         } else {
             showToast("Listener is null ON CREATEVIEW");
         }
     }
 
     private void handleDeleteButtonClick(View v) {
-        if (listener != null) {
-            listener.onDeleteButtonClick();
+        if (mListener != null) {
+            mListener.onDeleteButtonClick();
         } else {
             showToast("Listener is null ON CREATEVIEW");
         }
